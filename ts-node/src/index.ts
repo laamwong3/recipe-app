680537fd847e2015672b99cb4ced1e2e20c85606
Laam Wong <laamwong@Laams-iMac.local>
2022-08-06 20:29:51 +0800

    fetch data

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -2,8 +2,7 @@ import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config({ path: "../../.env.local" });
 import randomRecipes from "./constants/randomRecipes.json";
-
-// console.log(process.env.NEXT_PUBLIC_RECIPE_API_KEY);
+import randomDetails from "./constants/recipeDetails.json";
 
 const fetchRandomRecipes = () => {
   const url = `https://api.spoonacular.com/recipes/random?apiKey=${process.env.NEXT_PUBLIC_RECIPE_API_KEY}&number=20`;
@@ -18,16 +17,45 @@ const fetchRandomRecipes = () => {
     .catch((e) => console.log(e));
 };
 
-const fetchRecipeInfo = () => {
-  let tempArr = new Map();
-  randomRecipes.recipes.map((each) => {
-    const url = `https://api.spoonacular.com/recipes/${each.id}/information?apiKey=${process.env.NEXT_PUBLIC_RECIPE_API_KEY}&includeNutrition=false`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {})
-      .catch((e) => console.log(e));
+const fetchRecipeInfo = async () => {
+  let tempArr = new Map<number, {}>();
+
+  await Promise.all(
+    randomRecipes.recipes.map(async (recipe) => {
+      const url = `https://api.spoonacular.com/recipes/${recipe.id}/information?apiKey=${process.env.NEXT_PUBLIC_RECIPE_API_KEY}&includeNutrition=false`;
+      let response = await fetch(url);
+      let data = await response.json();
+      tempArr.set(recipe.id, data);
+    })
+  );
+  fs.writeFileSync(
+    "./constants/recipeDetails.json",
+    JSON.stringify([...tempArr], null, 2)
+  );
+
+  randomDetails.map((each) => {
+    each.map((e) => {
+      console.log(e);
+    });
   });
+  // for (let each of tempArr) {
+  //   console.log(each);
+  // }
 };
 
+// const testMapping = async () => {
+//   let arr = [1, 2, 3, 4, 5, 6, 7];
+//   let tempArr = new Map<number, {}>();
+//   await Promise.all(
+//     arr.map((each) => {
+//       tempArr.set(each, { a: 10 });
+//     })
+//   );
+//   console.log(tempArr.values);
+// };
+
 // fetchRandomRecipes();
-fetchRecipeInfo();
+
+(async () => {
+  await fetchRecipeInfo();
+})();
